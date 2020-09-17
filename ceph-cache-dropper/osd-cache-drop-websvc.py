89e9b603f679a0ce628af8132e8a16f6930eeeeb
Ben England <bengland@redhat.com>
2020-09-17 18:32:34 -0400

    do not be the toolbox pod, just talk to it use kubectl exec to do this env vars supply toolbox pod namespace and id make service port number configurable as env var do not catch exceptions, so that return code is -1 from wget cherry-py will handle them gracefully

--- ceph-cache-dropper/osd-cache-drop-websvc.py
+++ ceph-cache-dropper/osd-cache-drop-websvc.py
@@ -1,13 +1,5 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
 # Ceph OSD cache dropping service
-# must run as privileged pod, but pods that call it
-# do not have to, and more than one workload can make use of it
-# to use it:
-# # oc create -f dropper.yml
-# # drop_pod_ip=$(oc -n openshift-storage get pod -o wide | awk '/drop/{print $1}')
-# # curl http://$drop_pid_ip:9432/drop_osd_caches
-# SUCCESS
 
 import subprocess
 import logging
@@ -16,15 +8,36 @@ import atexit
 import os
 import sys
 
-#logging.basicConfig(filename='/tmp/dropcache.log', level=logging.DEBUG)
-logging.basicConfig(level=logging.DEBUG)
+# process exit status
+NOTOK = 1
+
+logging_format = '%(asctime)-15s %(message)s'
+logging.basicConfig(format=logging_format, level=logging.DEBUG)
 logger = logging.getLogger('dropcache')
-logger.setLevel(logging.DEBUG)
+
 
 def flush_log():
     logging.shutdown()
 
+
 atexit.register(flush_log)
+
+# parse environment variables and log them
+
+svcPortNum = 9435
+portnumstr = os.getenv('CEPH_CACHE_DROP_PORT_NUM')
+if portnumstr is not None:
+    svcPortNum = int(portnumstr)
+toolbox_pod_name = os.getenv('CEPH_TOOLBOX_POD_ID')
+if toolbox_pod_name is None:
+    logger.error('no CEPH_TOOLBOX_POD_ID env. var., shutting down')
+    sys.exit(NOTOK)
+toolbox_pod_namespace = os.getenv('CEPH_TOOLBOX_POD_NAMESPACE')
+if toolbox_pod_namespace is None:
+    toolbox_pod_namespace = 'openshift-storage'
+logger.info('ceph cache drop port num %d, toolbox pod ID %s, namespace %s' %
+            (svcPortNum, toolbox_pod_name, toolbox_pod_namespace))
+
 
 class DropOSDCache(object):
     @cherrypy.expose
@@ -33,33 +46,21 @@ class DropOSDCache(object):
 
     @cherrypy.expose
     def drop_osd_caches(self):
-        try:
-            result = subprocess.check_output(
-                ["/usr/bin/ceph", "tell", "osd.*", "cache", "drop"])
-            logger.debug(result)
-        except subprocess.CalledProcessError as e:
-            logger.error('failed to drop cache')
-            logger.exception(e)
-            return 'FAIL'
+        result = subprocess.check_output(
+                ['/opt/ceph_cache_drop/kubectl', '-n', toolbox_pod_namespace,
+                 'exec', '-i', '-t', toolbox_pod_name, '--',
+                 "/usr/bin/ceph", "tell", "osd.*", "cache", "drop"])
+        logger.debug(result)
         return 'SUCCESS'
 
-if __name__ == '__main__':
-    try:
-        result = subprocess.Popen(
-            ["/bin/sh", "-c", "/usr/local/bin/toolbox.sh"])
-    except subprocess.CalledProcessError as e:
-        logger.error('failed to source toolbox')
-        logger.exception(e)
-        sys.exit(3)
-    if not os.path.exists('/etc/ceph/ceph.conf'):
-        logger.error('DID NOT FIND ceph.conf')
-        sys.exit(2)
-    logger.info('entering service')
 
-    config = { 
+if __name__ == '__main__':
+    logger.info('entering OSD cache drop service')
+
+    config = {
         'global': {
-            'server.socket_host': '0.0.0.0' ,
-            'server.socket_port': 9432,
+            'server.socket_host': '0.0.0.0',
+            'server.socket_port': svcPortNum,
         },
     }
     cherrypy.config.update(config)
